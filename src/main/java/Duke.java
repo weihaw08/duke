3217e058d6c848ce64bdb005580076b7b9a39328
weihaw08 <weihaw08@yahoo.com.sg>
2019-08-17 22:21:10 +0800

    Add level-1 features: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String BORDER = "    ____________________________________________________________";
+    private static boolean isBye = false;
+
+    private static void printBorder() {
+        System.out.println(BORDER);
+    }
+
+    private static void printMessage(String s) {
+        printBorder();
+        System.out.println("    " + s);
+        printBorder();
+    }
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printBorder();
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        printBorder();
+
+        while(!isBye) {
+            String command = input.next();
+            if (command.equals("bye")) {
+                isBye = true;
+                printMessage("Bye. Hope to see you again soon!");
+            } else {
+                printMessage(command);
+            }
+        }
+
+        input.close();
     }
 }

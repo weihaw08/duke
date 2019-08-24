b616d24b562b9894050f1a4a681b3fd73ff0febe
weihaw08 <weihaw08@yahoo.com.sg>
2019-08-24 23:53:17 +0800

    Add Level-7 Ability to save tasks in list

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -6,9 +6,19 @@ public class Deadline extends Task {
         this.deadline = deadline;
     }
 
+    Deadline(String taskName, boolean isDone, String deadline) {
+        super(taskName, isDone);
+        this.deadline = deadline;
+    }
+
     @Override
     public String toString() {
         String sym = this.isDone ? "\u2713" : "\u2717";
         return "[D]" + "[" + sym + "] " + this.taskName + " (by: " + this.deadline + ")";
+    }
+
+    @Override
+    protected String convertToText() {
+        return "D" + " ~ " + this.isDone + " ~ " + this.taskName + " ~ " + this.deadline;
     }
 }
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,12 +1,21 @@
 public class ToDo extends Task {
 
-    ToDo(String toDoName) {
-        super(toDoName);
+    ToDo(String taskName) {
+        super(taskName);
+    }
+
+    ToDo(String taskName, boolean isDone) {
+        super(taskName, isDone);
     }
 
     @Override
     public String toString() {
         String sym = this.isDone ? "\u2713" : "\u2717";
         return "[T]" + "[" + sym + "] " + this.taskName;
+    }
+
+    @Override
+    protected String convertToText() {
+        return "T" + " ~ " + this.isDone + " ~ " + this.taskName;
     }
 }

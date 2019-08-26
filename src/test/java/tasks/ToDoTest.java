2475d8ac82375cfa785f46c16a59004d1873aded
weihaw08 <weihaw08@yahoo.com.sg>
2019-08-26 23:56:47 +0800

    Add JUnit Testing for ToDo task.

---
+++ src/test/java/tasks/ToDoTest.java
@@ -0,0 +1,31 @@
+package tasks;
+
+import duke.tasks.ToDo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ToDoTest {
+
+    @Test
+    void testStringConversion() {
+        // Testing string conversion for constructor 1
+        assertEquals("[T][✗] sleep", new ToDo("sleep").toString());
+
+        // Testing string conversion for constructor 2
+        assertEquals("[T][✓] talk", new ToDo("talk", true).toString());
+    }
+
+    @Test
+    void testIsDoneFunction() {
+        ToDo t = new ToDo("sleep");
+        t.markAsDone();
+        assertEquals("[T][✓] sleep", t.toString());
+    }
+
+    @Test
+    void testTextConversion() {
+        assertEquals("T ~ false ~ sleep", new ToDo("sleep").convertToText());
+        assertEquals("T ~ true ~ talk", new ToDo("talk", true).convertToText());
+    }
+}
